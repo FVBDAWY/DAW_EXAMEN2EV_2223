4fd63e9c51d97c00eab716a3d268977e2e133252
Francisco Vicedo Blasco <fravicbla@alu.edu.gva.es>
2023-03-16 18:50:27 +0100

    Documentacion del fichero de loto

--- Loto/Loto/LotoFVB1Y.cs
+++ Loto/Loto/LotoFVB1Y.cs
@@ -2,25 +2,48 @@
 
 namespace LotoClassNS
 {
-    // Clase que almacena una combinación de la lotería
-    //
+    
+    /// <summary>
+    /// Clase que almacena una combinación de la lotería
+    /// </summary>
     public class loto
     {
-        // definición de constantes
-        public const int MAX_NUMEROS = 6;
-        public const int NUMERO_MENOR = 1;
-        public const int NUMERO_MAYOR = 49;
         
-        private int[] _nums = new int[MAX_NUMEROS];   // numeros de la combinación
-        public bool ok = false;      // combinación válida (si es aleatoria, siempre es válida, si no, no tiene porqué)
-
+        /// <summary>
+        /// L constante define que el maximo de numeros a introducir seran 6
+        /// </summary>
+        public const int MAX_NUMEROS = 6;
+        /// <summary>
+        /// La constante define que el numero maximo que se podra introducir sera el 1
+        /// </summary>
+        public const int NUMERO_MENOR = 1;
+        /// <summary>
+        /// La constante define que el maximo numero que se podra introducir sera el 49
+        /// </summary>
+        public const int NUMERO_MAYOR = 49;
+        /// <summary>
+        /// Un array con los numeros de la combinacion
+        /// </summary>
+        private int[] _nums = new int[MAX_NUMEROS];
+        /// <summary>
+        /// combinación válida (si es aleatoria, siempre es válida, si no, no tiene porqué)
+        /// </summary>
+        public bool ok = false;   
+        ///<value>
+        /// Recibe el valor de los numeros
+        /// </value>
         public int[] Nums { 
             get => _nums; 
             set => _nums = value; 
         }
 
-        // En el caso de que el constructor sea vacío, se genera una combinación aleatoria correcta
-        //
+       
+        
+        /// <summary>
+        /// El construnctor genera numeros aleatorios
+        /// </summary>
+        ///<remarks>Se genera automaticamente,En el caso de que el constructor sea vacío, se genera una combinación aleatoria correcta</remarks>
+        ///<returns>Te devolvera un true una vez generado con exito</returns>
         public loto()
         {
             Random r = new Random();    // clase generadora de números aleatorios
@@ -43,8 +66,12 @@ namespace LotoClassNS
             ok=true;
         }
 
-        // La segunda forma de crear una combinación es pasando el conjunto de números
-        // misnums es un array de enteros con la combinación que quiero crear (no tiene porqué ser válida)
+        
+        /// <summary>
+        /// La segunda forma de crear una combinación es pasando el conjunto de números
+        // <remarks>misnums es un array de enteros con la combinación que quiero crear (no tiene porqué ser válida)</remarks>
+        /// </summary>
+        /// <param name="misnums">Define combinacion con la que queremos inicializar la clase</param>
         public loto(int[] misnums)  // misnumeros: combinación con la que queremos inicializar la clase
         {
             for (int i=0; i<MAX_NUMEROS; i++)
@@ -68,9 +95,12 @@ namespace LotoClassNS
 	    ok=true;
         }
 
-        // Método que comprueba el número de aciertos
-        // premi es un array con la combinación ganadora
-        // se devuelve el número de aciertos
+        
+        /// <summary>
+        /// Metodo que comprobara el numero de aciertos
+        /// </summary>
+        /// <param name="premi">Define el array con la combinacion ganadora</param>
+        /// <returns>Se devuelve el numero de aciertos</returns>
         public int comprobar(int[] premi)
         {
             int a=0;                    // número de aciertos
